6fd8eaca271b5be50431e874917b0d5b467520b9
shutnikPk <shutnikssspk@gmail.com>
2021-09-06 16:30:54 +0300

    add markUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="calculator">
+        <div className="input">0</div>
+        <div className="buttons">
+          <div className="operators">
+            <div>+</div>
+            <div>-</div>
+            <div>*</div>
+            <div>/</div>
+          </div>
+          <div className="leftPanel">
+            <div className="numbers">
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+            </div>
+            <div className="numbers">
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+            </div>
+            <div className="numbers">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+            </div>
+            <div className="numbers">
+              <div>0</div>
+              <div>.</div>
+              <div>AC</div>
+            </div>
+          </div>
+          <div className="equal">=</div>
+        </div>
+      </div>
     </div>
   );
 }
